5682cd804735da5488064c4b65f387f2734c3345
Maico <maicoloncomilla@gmail.com>
2021-03-03 18:20:29 -0300

    Updates

---
+++ precache-manifest.37176e4cc13be3fc2af0554d997ab680.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2eaa1e40530c4f3ade127140ae2b08",
+    "url": "/Trello/index.html"
+  },
+  {
+    "revision": "f0574377ed365ede78d1",
+    "url": "/Trello/static/css/main.e55f3b1d.chunk.css"
+  },
+  {
+    "revision": "e1da6d894f4d59ab2950",
+    "url": "/Trello/static/js/2.ae003324.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Trello/static/js/2.ae003324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0574377ed365ede78d1",
+    "url": "/Trello/static/js/main.04f5366e.chunk.js"
+  },
+  {
+    "revision": "1dc6264eacd9ac551116",
+    "url": "/Trello/static/js/runtime-main.eb3c2a61.js"
+  },
+  {
+    "revision": "4788048f319dc48101678d9e69f5077e",
+    "url": "/Trello/static/media/emptyAvatar.4788048f.png"
+  }
+]);
